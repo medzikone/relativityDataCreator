5d75899d1fe7274996c1a8c1de24885593057ace
Filip Wrona <filip.wrona@relativity.com>
2019-09-02 18:49:36 +0200

    message about documents fix

--- DataSetsforSyncPerformance/Scripts/DataCreator.ps1
+++ DataSetsforSyncPerformance/Scripts/DataCreator.ps1
@@ -6,6 +6,7 @@ $fixOrRandom = Read-Host -Prompt "Do you want use random or fixed size files?: [
 If ($fixOrRandom -eq 'R'){
 
     #input minimum and maxiumum file size
+    Write-Host "If you want to create all of supported documents type '[A]ll' or number greater than 138"
     $numberofDocuments = Read-Host "How many uniqe random docs do you want to create?: "
     Write-Host "Minimum has to be less than Maxiumum"
 
@@ -20,7 +21,6 @@ If ($fixOrRandom -eq 'R'){
     until ($minimumSizeFileRandom -ge 1 -and $maximumSizeFileRandom -ge 1 -and ($minimumSizeFileRandom -lt $maximumSizeFileRandom) -and $numOK)
   
     }
-    Write-Host "If you want to create all of supported documents type '[A]ll' or number greater than 138"
     
     $createdDocuments = 1
     If ($numberofDocuments -eq "A"){
